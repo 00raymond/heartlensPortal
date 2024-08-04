0caa5f7d5c4c5a3989590c8104dc970b625dab00
00raymond <raymondhouse2003@gmail.com>
2024-08-03 23:43:28 -0700

    admin dashboard

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -35,7 +35,7 @@ const AdminDashboard = () => {
                 <h1 className="font-semibold text-2xl">Admin Dashboard</h1>
                 <button onClick={logout} className="p-2 border-xl border-white border-2 rounded-xl hover:bg-opacity-25 hover:bg-white transition-all duration-200">Logout</button>
             </div>
-            <div className="flex flex-row justify-center items-center">
+            <div className="flex flex-row justify-center items-center space-x-3">
                 <div className="rounded-xl bg-white bg-opacity-25 p-4 w-[500px] h-[500px]">
                     <div className="flex flex-row space-x-5 items-center pb-2">
                         <h2 className="font-semibold text-xl">User Moderation</h2>
@@ -46,6 +46,10 @@ const AdminDashboard = () => {
                         </div>
                     </div>
                     <UserList userType={selectedModeration} />
+                </div>
+                <div className="rounded-xl bg-white bg-opacity-25 p-4 w-[500px] h-[500px]">
+                    <h1>Create User</h1>
+
                 </div>
             </div>
         </div>
